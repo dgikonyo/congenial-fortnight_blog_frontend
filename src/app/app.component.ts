132389178d607048dd5b797a3aeb75cc7bc081d2
Kimani <KEN22934@kcbgroup.com>
2024-01-29 16:27:19 +0300

    added groups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Group } from './models/group';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'blog_tutorial';
+  loading = true;
+  groups: Group[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loading = true;
+    this.http.get<Group[]>('api/groups').subscribe((data: Group[]) => {
+      this.groups = data;
+      this.loading = false;
+    });
+  }
 }
