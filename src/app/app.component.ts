0c17f5987a4e05333cfb2b2cbf603a4fedcdae0a
Kimani <KEN22934@kcbgroup.com>
2024-01-30 15:53:24 +0300

    added group lists

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Group } from './models/group';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +9,6 @@ import { Group } from './models/group';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
-  title = 'blog_tutorial';
-  loading = true;
-  groups: Group[] = [];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.loading = true;
-    this.http.get<Group[]>('api/groups').subscribe((data: Group[]) => {
-      this.groups = data;
-      this.loading = false;
-    });
-  }
+export class AppComponent {
+  title = 'Baraza';
 }
---
+++ src/app/group-list/group-list.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { Group } from '../models/group';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-group-list',
+  standalone: true,
+  imports: [
+    RouterLink,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    DatePipe,
+    HttpClientModule,
+  ],
+  templateUrl: './group-list.component.html',
+  styleUrl: './group-list.component.css',
+})
+export class GroupListComponent {
+  title = 'Group List';
+  loading = false;
+  groups: Group[] = [];
+  displayedColumns = ['id', 'name', 'events', 'actions'];
+  feedback: any = {};
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loading = true;
+    this.http.get<Group[]>('api/groups').subscribe((data: Group[]) => {
+      this.groups = data;
+      this.loading = false;
+      this.feedback = {};
+    });
+  }
+
+  delete(group: Group): void {
+    if (confirm(`Are you sure you want to delete ${group.name}?`)) {
+      this.http.delete(`api/group/${group.id}`).subscribe({
+        next: () => {
+          this.feedback = { type: 'success', message: 'Delete was successful' };
+          setTimeout(() => {
+            this.ngOnInit();
+          }, 1000);
+        },
+        error: () => {
+          this.feedback = { type: 'warning', message: 'Error deleteing.' };
+        },
+      });
+    }
+  }
+  //protected readonly event = event;
+}
