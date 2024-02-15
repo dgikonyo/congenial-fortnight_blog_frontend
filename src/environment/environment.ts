b58d14c5be295ec2efacc08e2162a8d1db4f69c7
Kimani <KEN22934@kcbgroup.com>
2024-02-15 16:05:02 +0300

    edited env

---
+++ src/environment/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiKey: 'devKey',
+  targetBackendAPI:'http://localhost:8080/',
+  OKTA_OAUTH2_ISSUER:'https://dev-b71qyu647q06c1zb.us.auth0.com/',
+};
